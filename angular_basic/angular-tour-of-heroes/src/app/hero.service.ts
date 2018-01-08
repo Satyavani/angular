7da6f4bbe299fd7bd16df7324bcc25a90f99d5b7
Satyavani <5885828+Satyavani@users.noreply.github.com>
2018-01-08 11:27:18 +0530

    intermediate commit

--- angular_basic/angular-tour-of-heroes/src/app/hero.service.ts
+++ angular_basic/angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,27 +1,48 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import {Hero} from './hero';
-import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     // Todo: send the mesage _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    this.log('fetched heroes');
+    return this.http.get<Hero[]>(this.heroesUrl).pipe( catchError(this.handleError('getHeroes', [])));
   }
 
   getHero(id: number): Observable<Hero> {
     // Todo: send the messahe after fetching the hero
-    this.messageService.add(`HeroService: fetched hero: ${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    this.log(`fetched hero: ${id}`);
+    return this.http.get<Hero[]>(this.heroesUrl).find(hero => hero.id === id));
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T> (operation = `operation`, result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to standard log framework
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning empty result.
+      return of(result as T);
+    }
   }
 
 }
